7750091ecd9c264b50acb7d703c50f82f064591b
Andrew Lloyd <andrewglloyd@hotmail.com>
2014-10-16 21:43:17 +1100

    Initial Commit

---
+++ api/events/index.php
@@ -0,0 +1,3 @@
+<?php
+echo '1001|2014-10-10 16:00:00|Adelaide 3 Day Eventing|/images/event_logo_1.png|Welcome to Adelaide 3 Day Eventing. Please ensure that your contact details are correct and submitted at least 30 minutes prior to your event.';
+?>
